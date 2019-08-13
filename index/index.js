200cbc9e59cb2bae6de2b19e0e42c6c6416c869b
wusiqing <siqingiii@163.com>
2019-08-13 15:25:28 +0800

    gaa

--- index/index.js
+++ index/index.js
@@ -1,8 +1,4 @@
 console.log('公共文件')
-<<<<<<< HEAD
 console.log('feature-u u u u u ')
 console.log('我新建了 feature-A ，新增了一个 log')
-=======
-console.log('我新建了 feature-A ，新增了一个 log')
 console.log('我创建了分支')
->>>>>>> feature-B rebase’
